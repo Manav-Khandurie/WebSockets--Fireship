7ad2ae126dcdc4522169cb75a5aa3009ef4ce239
Manav Khandurie <manavkhandurie@gmail.com>
2023-09-25 17:11:50 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -2,45 +2,46 @@ provider "aws" {
   region = "us-east-1"  # Change this to your desired region
 }
 
+resource "aws_security_group" "example" {
+  name        = "example-security-group"
+  description = "Example security group for SSH, TCP, UDP, HTTP, and HTTPS"
+  
+  # Ingress rule for SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere (not recommended for production)
+  }
+
+  # Ingress rule for TCP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
+  }
+
+  # Ingress rule for HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS from anywhere
+  }
+
+  # Ingress rule for UDP
+  ingress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow DNS (UDP) from anywhere
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-053b0d53c279acc90"  # Change this to the desired AMI ID
   instance_type = "t2.micro"  # Change this to your desired instance type
-  user_data = <<-EOF
-  #!/bin/bash 
-# setting up the server for installing nginx and nodejs
-sudo su
-
-apt update
-
-apt install nginx -y 
-#installing nginx at the server
-curl -s https://deb.nodesource.com/setup_16.x | sudo bash 
-
-apt install nodejs -y
-#installing node js at the server
-git clone -b master2 https://github.com/Manav-Khandurie/AWSNETFLIXSAAS.git
-
-npm install pm2@latest -g
-#installing pm2 at the server
-cd AWSNETFLIXSAAS/netflix-api
-
-npm install
-#installs dependencies
-cd ..
-
-cd checking
-
-rm /etc/nginx/sites-available/default
-
-mv default /etc/nginx/sites-available/default
-#adds the custom ngnix cong file
-cd ..
-
-cd netflix-api
-
-pm2 start server.js --name netflix 
-#starts SAAS application
-sudo service nginx restart
-#restarts the service
-  EOF
+  user_data = 
+ 
 }
